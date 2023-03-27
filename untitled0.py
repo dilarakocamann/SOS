c94506d6998825309c611a1de4ba8ea950d28300
Dilara Kocaman <114511795+dilarakocamann@users.noreply.github.com>
2023-03-27 23:31:48 +0300

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -1,157 +1,129 @@
-from math import inf as infinity
+import random
 
-board = [[' ',' ',' '],
-         [' ',' ',' '],
-         [' ',' ',' ']]
+print("Welcome to Tic Tac Toe")
+print("----------------------")
 
-players = ['A','B']
+possibleNumbers = [1,2,3,4,5,6,7,8,9]
+gameBoard = [[1,2,3], [4,5,6], [7,8,9]]
+rows = 3
+cols = 3
 
-def play_move(state, player, block_num):
-    if state[int((block_num-1)/3)][(block_num-1)%3] == ' ':
-        state[int((block_num-1)/3)][(block_num-1)%3] = player
+def printGameBoard():
+  for x in range(rows):
+    print("\n+---+---+---+")
+    print("|", end="")
+    for y in range(cols):
+      print("", gameBoard[x][y], end=" |")
+  print("\n+---+---+---+")
+
+def modifyArray(num, turn):
+  num -= 1
+  if(num == 0):
+    gameBoard[0][0] = turn
+  elif(num == 1):
+    gameBoard[0][1] = turn
+  elif(num == 2):
+    gameBoard[0][2] = turn
+  elif(num == 3):
+    gameBoard[1][0] = turn
+  elif(num == 4):
+    gameBoard[1][1] = turn
+  elif(num == 5):
+    gameBoard[1][2] = turn
+  elif(num == 6):
+    gameBoard[2][0] = turn
+  elif(num == 7):
+    gameBoard[2][1] = turn
+  elif(num == 8):
+    gameBoard[2][2] = turn
+
+### Define function to check for a winner
+def checkForWinner(gameBoard):
+  
+  ### X axis
+  if(gameBoard[0][0] == 'X' and gameBoard[0][1] == 'X' and gameBoard[0][2] == 'X'):
+    print("X has won!")
+    return "X"
+  elif(gameBoard[0][0] == 'O' and gameBoard[0][1] == 'O' and gameBoard[0][2] == 'O'):
+    print("O has won!")
+    return "O"
+  elif(gameBoard[1][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[1][2] == 'X'):
+    print("X has won!")
+    return "X"
+  elif(gameBoard[1][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[1][2] == 'O'):
+    print("O has won!")
+    return "O"
+  elif(gameBoard[2][0] == 'X' and gameBoard[2][1] == 'X' and gameBoard[2][2] == 'X'):
+    print("X has won!")
+    return "X"
+  elif(gameBoard[2][0] == 'O' and gameBoard[2][1] == 'O' and gameBoard[2][2] == 'O'):
+    print("O has won!")
+    return "O"
+
+  ### Y axis
+  if(gameBoard[0][0] == 'X' and gameBoard[1][0] == 'X' and gameBoard[2][0] == 'X'):
+    print("X has won!")
+    return "X"
+  elif(gameBoard[0][0] == 'O' and gameBoard[1][0] == 'O' and gameBoard[2][0] == 'O'):
+    print("O has won!")
+    return "O"
+  elif(gameBoard[0][1] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][1] == 'X'):
+    print("X has won!")
+    return "X"
+  elif(gameBoard[0][1] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][1] == 'O'):
+    print("O has won!")
+    return "O"
+  elif(gameBoard[0][2] == 'X' and gameBoard[1][2] == 'X' and gameBoard[2][2] == 'X'):
+    print("X has won!")
+    return "X"
+  elif(gameBoard[0][2] == 'O' and gameBoard[1][2] == 'O' and gameBoard[2][2] == 'O'):
+    print("O has won!")
+    return "O"
+
+  ### Cross wins
+  elif(gameBoard[0][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][2] == 'X'):
+    print("X has won!")
+    return "X"
+  elif(gameBoard[0][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][2] == 'O'):
+    print("O has won!")  
+    return "O"
+  elif(gameBoard[0][2] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][0] == 'X'):
+    print("X has won!")  
+    return "X"
+  elif(gameBoard[0][2] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][0] == 'O'):
+    print("O has won!") 
+    return "O" 
+  else:
+    return "N"
+  
+leaveLoop = False
+turnCounter = 0
+
+while(leaveLoop == False):
+         
+  ### It's the player turn
+  if(turnCounter % 2 == 0):
+    printGameBoard()
+    numberPicked = int(input("\nChoose a number [1-9]: "))
+    if(numberPicked >= 1 or numberPicked <= 9):
+      modifyArray(numberPicked, 'X')
+      possibleNumbers.remove(numberPicked)
     else:
-        block_num = int(input("Block is not empty, Choose again: "))
-        play_move(state, player, block_num)
-    
-def copy_game_state(state):
-    new_state = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
-    for i in range(3):
-        for j in range(3):
-            new_state[i][j] = state[i][j]
-    return new_state
-    
-def check_current_state(game_state):
-    
-    
-    # Check horizontals
-    if (game_state[0][0] == game_state[0][1] and game_state[0][1] == game_state[0][2] and game_state[0][0] != ' '):
-        return game_state[0][0], "Done"
-    if (game_state[1][0] == game_state[1][1] and game_state[1][1] == game_state[1][2] and game_state[1][0] != ' '):
-        return game_state[1][0], "Done"
-    if (game_state[2][0] == game_state[2][1] and game_state[2][1] == game_state[2][2] and game_state[2][0] != ' '):
-        return game_state[2][0], "Done"
-    
-    # Check verticals
-    if (game_state[0][0] == game_state[1][0] and game_state[1][0] == game_state[2][0] and game_state[0][0] != ' '):
-        return game_state[0][0], "Done"
-    if (game_state[0][1] == game_state[1][1] and game_state[1][1] == game_state[2][1] and game_state[0][1] != ' '):
-        return game_state[0][1], "Done"
-    if (game_state[0][2] == game_state[1][2] and game_state[1][2] == game_state[2][2] and game_state[0][2] != ' '):
-        return game_state[0][2], "Done"
-    
-    # Check diagonals
-    if (game_state[0][0] == game_state[1][1] and game_state[1][1] == game_state[2][2] and game_state[0][0] != ' '):
-        return game_state[1][1], "Done"
-    if (game_state[2][0] == game_state[1][1] and game_state[1][1] == game_state[0][2] and game_state[2][0] != ' '):
-        return game_state[1][1], "Done"
-    
-    # Check if draw
-    draw_flag = 0
-    for i in range(3):
-        for j in range(3):
-            if game_state[i][j] == ' ':
-                draw_flag = 1
-    if draw_flag == 0:
-        return None, "Draw"
-    
-    return None, "Not Done"
-
-def print_board(game_state):
-    print('----------------')
-    print('| ' + str(game_state[0][0]) + ' || ' + str(game_state[0][1]) + ' || ' + str(game_state[0][2]) + ' |')
-    print('----------------')
-    print('| ' + str(game_state[1][0]) + ' || ' + str(game_state[1][1]) + ' || ' + str(game_state[1][2]) + ' |')
-    print('----------------')
-    print('| ' + str(game_state[2][0]) + ' || ' + str(game_state[2][1]) + ' || ' + str(game_state[2][2]) + ' |')
-    print('----------------')
-    
-    
-def getBestMove(state, player):
-    '''
-    Minimax Algorithm
-    '''
-    winner_loser , done = check_current_state(state)
-    if done == "Done" and winner_loser == 'O': # If AI won
-        return (1,0)
-    elif done == "Done" and winner_loser == 'X': # If Human won
-        return (-1,0)
-    elif done == "Draw":    # Draw condition
-        return (0,0)
-        
-    moves = []
-    empty_cells = []
-    for i in range(3):
-        for j in range(3):
-            if state[i][j] == ' ':
-                empty_cells.append(i*3 + (j+1))
-    
-    for empty_cell in empty_cells:
-        move = {}
-        move['index'] = empty_cell
-        new_state = copy_game_state(state)
-        play_move(new_state, player, empty_cell)
-        
-        if player == 'O':    # If AI
-            result,_ = getBestMove(new_state, 'X')    # make more depth tree for human
-            move['score'] = result
-        else:
-            result,_ = getBestMove(new_state, 'O')    # make more depth tree for AI
-            move['score'] = result
-        
-        moves.append(move)
-
-    # Find best move
-    best_move = None
-    if player == 'O':   # If AI player
-        best = -infinity
-        for move in moves:            
-            if move['score'] > best:
-                best = move['score']
-                best_move = move['index']
-    else:
-        best = infinity
-        for move in moves:
-            if move['score'] < best:
-                best = move['score']
-                best_move = move['index']
-                
-    return (best, best_move)
-
-# PLaying
-play_again = 'Y'
-while play_again == 'Y' or play_again == 'y':
-    game_state = [[' ',' ',' '],
-              [' ',' ',' '],
-              [' ',' ',' ']]
-    current_state = "Not Done"
-    print("\nNew Game!")
-    print_board(game_state)
-    player_choice = input("Choose which player goes first - A (You) or B(The AI): ")
-    winner = None
-    
-    if player_choice == 'A' or player_choice == 'B':
-        current_player_idx = 0
-    else:
-        current_player_idx = 1
-        
-    while current_state == "Not Done":
-        if current_player_idx == 0: # Human's turn
-            block_choice = int(input("Your turn! Choose where to place (1 to 9): "))
-            play_move(game_state ,players[current_player_idx], block_choice)
-        else:   # AI's turn
-            _,block_choice = getBestMove(game_state, players[current_player_idx])
-            play_move(game_state ,players[current_player_idx], block_choice)
-            print("AI plays move: " + str(block_choice))
-        print_board(game_state)
-        winner, current_state = check_current_state(game_state)
-        if winner is not None:
-            print(str(winner) + " won!")
-        else:
-            current_player_idx = (current_player_idx + 1)%2
-        
-        if current_state == "Draw":
-            print("Draw!")
-            
-    play_again = input('Wanna try again?(Y/N) : ')
-    if play_again == 'N':
-        print('GG!')
+      print("Invalid input. Please try again.")
+    turnCounter += 1
+         
+  ### It's the computer's turn
+  else:
+    while(True):
+      cpuChoice = random.choice(possibleNumbers)
+      print("\nCpu choice: ", cpuChoice)
+      if(cpuChoice in possibleNumbers):
+        modifyArray(cpuChoice, 'O')
+        possibleNumbers.remove(cpuChoice)
+        turnCounter += 1
+        break
+  
+  winner = checkForWinner(gameBoard)
+  if(winner != "N"):
+    print("\nGame over!")
+    break
